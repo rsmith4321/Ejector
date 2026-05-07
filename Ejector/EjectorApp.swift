1058c7405933318d4fa256380eac56751859f158
Ryan Smith <rsmith4321@gmail.com>
2026-05-06 22:06:51 -0400

    Initial Commit

---
+++ Ejector/EjectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjectorApp.swift
+//  Ejector
+//
+//  Created by Ryan Smith on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct EjectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
